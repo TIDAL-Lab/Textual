df9c4460f70ff41a928855b0c4bb02d8c4a0c6a2
Michael Horn <michael-horn@northwestern.edu>
2017-06-21 18:16:00 -0500

    ave and restore with JSON

--- example/web/main.dart
+++ example/web/main.dart
@@ -21,8 +21,12 @@ import 'package:Textual/textual.dart';
 void main() {
   Program program = new Program("#content");
   HttpRequest.getString("blocks.json").then((jsonString) {
-    program.initMenu(JSON.decode(jsonString));
+    program.loadStatementDefinitions(JSON.decode(jsonString));
+    jsonString = '[{"id":1,"name":"beat","children":[{"id":22,"name":"strum"},{"id":23,"name":"slap"},{"id":24,"name":"with","params":[{"id":6,"name":null,"unit":"","value":"softer"}],"children":[{"id":26,"name":"rest","params":[{"type":"range","id":7,"name":"beats","unit":"","value":4}]},{"id":27,"name":"chance","params":[{"type":"range","id":8,"name":"value","unit":"%","value":54}],"children":[{"id":31,"name":"stop"}]}]}]}]';
+    program.fromJSON(JSON.decode(jsonString));
+    program.traceStatement(23);
   });
+
 
   program.onProgramChanged = () {
     print(JSON.encode(program.toJSON()));
