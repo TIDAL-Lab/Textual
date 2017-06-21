d8963804915946f8ef49d1951bb20ed792d6934e
Michael Horn <michael-horn@northwestern.edu>
2017-06-21 12:35:39 -0500

    program no longer implements block

--- lib/src/program.dart
+++ lib/src/program.dart
@@ -14,21 +14,24 @@
 part of Textual;
 
 
-/// Top level statement of a program that gets inserted into a container DIV
-class Program extends BeginStatement {
+/// Contains all of the program statements
+class Program {
 
 
   /// Name of the DIV tag the workspace is inserted into
   String containerId;
 
+  /// root elements for the program parse tree
+  List<Statement> children = new List<Statement>;
+
   /// List of statements that can be added to a program
-  List<Statement> _menu = new List<Statement>();
+  List<BeginStatement> _menu = new List<BeginStatement>();
 
 
   /// Create and insert a new workspace in the specified DIV tag [containerId]
-  Program(this.containerId)  : super("begin program", null) {
-    _end.name = "end program";
-    insertHtml();
+  Program(this.containerId) {
+    children.add(new BeginStatement("beat", null, this));
+    renderHtml();
   }
 
 
@@ -36,7 +39,7 @@ class Program extends BeginStatement {
   void loadStatements(List json) {
     for (var config in json) {
       if (config is Map) {
-        _menu.add(new Statement.fromJSON(config));
+        _menu.add(new Statement.fromJSON(config, this));
       }
     }
   }
@@ -44,16 +47,19 @@ class Program extends BeginStatement {
 
   /// Generate the HTML tags that get inserted into the parent DIV. 
   /// This is called automatically by the constructor
-  void insertHtml() {
+  void renderHtml() {
 
     // update program line numbers
-    int lineNum = updateLineNumbers(1);
-    _end.updateLineNumbers(lineNum + 1);
+    int lineNum = 1;
+    for (BeginStatement root in children) {
+      lineNum = root._updateLineNumbers(lineNum);
+      root._end.line = lineNum + 1;
+    }
 
     // create the new div tag
     DivElement div = new DivElement() .. className = "tx-program";
-    _renderHtml(div);
-    _end._renderHtml(div);
+    root._renderHtml(div);
+    root._end._renderHtml(div);
 
     // insert or replace the tx-program div
     DivElement container = querySelector("$containerId .tx-program");
@@ -66,33 +72,26 @@ class Program extends BeginStatement {
 
 
   /// Creates a statement pulldown menu below the [expander] element
-  void _openStatementMenu(Element expander, Statement after) {
+  void openStatementMenu(Element expander, Statement after) {
     querySelectorAll('.tx-pulldown-menu').style.display = "none";
     DivElement hmenu = new DivElement() .. classes.add('tx-pulldown-menu');
     for (Statement s in _menu) {
-      AnchorElement link = new AnchorElement();
-      link.innerHtml = "${s.name}";
-      link.href = "#";
+      AnchorElement link = new AnchorElement(href : "#") .. innerHtml = "${s.name}";
       if (s.hasParameters) link.innerHtml += " ... ";
       hmenu.append(link);
       link.onClick.listen((e) {
         hmenu.style.display = "none";
         hmenu.remove();
-        e.stopPropagation();
         Statement clone = s.clone();
         after.insertStatement(clone);
-        insertHtml();
+        renderHtml();
         clone._highlightLine();
+        e.stopPropagation();
       });
     }
     expander.append(hmenu);
     hmenu.style.display = "block";
   }
-
-
-  // don't allow dragging or selection for the begin program statement
-  void _highlightLine() { }
-
 }
 
 
